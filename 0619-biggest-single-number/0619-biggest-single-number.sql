90699df76cc5f2c2452afd03e3629374d69e7b59
Boufaied Rahma <151942213+rahmabfd@users.noreply.github.com>
2026-01-03 23:52:15 +0100

    Time: 376 ms (84.11%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 select MAX(num) as num
-from ( select num
 from MyNumbers
-group by num
-having count(*)=1) as singles
+where num in ( 
+    select num
+    from MyNumbers
+    group by num
+    having count(*)=1) 
